f7429cd16778179fd204a5aea7b00c3ab2ed28bc
Pratiksha Padhi <pratikshapadhi1997@gmail.com>
2016-08-04 20:29:58 +0530

    Update english.js

--- english.js
+++ english.js
@@ -3,13 +3,13 @@ var url = "https://api.nytimes.com/svc/topstories/v1/politics.json";
 url += '?' + $.param({
   'api-key': "#"
   });
-$.getJSON(url,function gotData(object){
+$.getJSON(url,function gotData(object){               //FUNCTION TO CREATE API’S
 		var headln=object.results
 		//for (var i=0;i<headln.length;i++)
 	//console.log(headln[0].abstract);/*
 	$("#news1").html("<h4>"+headln[0].abstract+"</h4>")
 			
-		 
+		 //RANDOM NEWS COROUSEL
 		 $("#news2").html("<h4>"+headln[Math.floor((Math.random() * headln.length) + 1)].abstract+"</h4>")
 		  $("#news3").html("<h4>"+headln[Math.floor((Math.random() * headln.length) + 1)].abstract+"</h4>")
 		
